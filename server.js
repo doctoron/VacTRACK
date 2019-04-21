0c7218814b9f5296c693e77ccee3a70bdd93a803
doctoron <ron.antonio@me.com>
2019-04-21 14:37:29 -0400

    server.js mongoose and routes added

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
