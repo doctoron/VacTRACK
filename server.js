ad1d8c0ca38baff5c8053a02792f067d592beedf
doctoron <ron.antonio@me.com>
2019-05-03 21:26:27 -0400

    backend connection seeded

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Members", { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
