e7853c7ff546ac88635da02d9917d909b20d9f26
craigz123 <czimbler60@gmail.com>
2019-05-07 20:30:12 -0400

    created a component in HIV.js

--- client/src/components/Register/female.js
+++ client/src/components/Register/female.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import Axios from 'axios';
 import { Button, Form, FormGroup, Label, Input, FormText, ListGroup, ListGroupItem, Card } from 'reactstrap';
 
 const Female = (props) => {
     return (
       <Form>
-        <FormGroup check>
+        <FormGroup check id="Female1">
           <Label check>
             <Input type="checkbox" />{' '}
             I could become pregnant
           </Label>
         </FormGroup>
-        <FormGroup check>
+        <FormGroup check id="Female2">
           <Label check>
             <Input type="checkbox" />{' '}
             I am pregnant now
           </Label>
         </FormGroup>
-        <FormGroup check>
+        <FormGroup check id="Female3">
           <Label check>
             <Input type="checkbox" />{' '}
             I am not planning to become pregnant
           </Label>
         </FormGroup>
-        . <FormGroup check>
+        . <FormGroup check id="Female4">
           <Label check>
             <Input type="checkbox" />{' '}
             I could not become pregnant
