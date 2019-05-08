83fde9c8a2d7b81617583018e58b6d7873c0b5b2
doctoron <ron.antonio@me.com>
2019-05-07 20:34:57 -0400

    Fixed nested form warning

--- client/src/components/Register/female.js
+++ client/src/components/Register/female.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import Axios from 'axios';
-import { Button, Form, FormGroup, Label, Input, FormText, ListGroup, ListGroupItem, Card } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const Female = (props) => {
-    return (
-      <Form>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />{' '}
-            I could become pregnant
+  return (
+    <div>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          I could become pregnant
           </Label>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />{' '}
-            I am pregnant now
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          I am pregnant now
           </Label>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />{' '}
-            I am not planning to become pregnant
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          I am not planning to become pregnant
           </Label>
-        </FormGroup>
-        . <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />{' '}
-            I could not become pregnant
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          I could not become pregnant
           </Label>
-        </FormGroup>
-      </Form>
-    );
-  };
-  
-  export default Female;
+      </FormGroup>
+    </div>
+  );
+};
+
+export default Female;
