37c507620ed4472c2e1a37db67465dcff547055e
doctoron <ron.antonio@me.com>
2019-05-14 17:40:46 -0400

    vaccine matrix

--- client/src/Pages/PVRec.js
+++ client/src/Pages/PVRec.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import vaccines from '../constants/vaccines';
 
 const PVRec = (props) => {
+
+    for (let i = 0; i < vaccines.length; i++) {
+        if(vaccines[i].hiv === true){
+            giveVaccine();
+        }
+        
+        if(vaccines[i].date >= 50){
+
+        } else if(vaccines[i].date <= 50){
+
+        }
+        
+    }
+
         return (
             <div>Your Personal Vaccination Record</div>
         )
---
+++ client/src/constants/vaccines.js
@@ -0,0 +1,6 @@
+let vaccines = [
+    { name='zosterLive', gender='both', hiv='true', date=50 },
+
+];
+
+export default vaccines;
