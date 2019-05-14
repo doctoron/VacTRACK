82e1cfdced452fab7e5ba9ff37f1bc09a2c5e455
craigz123 <czimbler60@gmail.com>
2019-05-14 18:38:06 -0400

    styling

--- client/src/components/Cards/cards.js
+++ client/src/components/Cards/cards.js
@@ -3,19 +3,13 @@ import {
   Card, Button, CardImg, CardTitle, CardText, CardGroup,
   CardSubtitle, CardBody
 } from 'reactstrap';
-import image1 from '../../img/jump.jpg'
-import image2 from '../../img/jump.jpg'
-import image3 from '../../img/jump.jpg'
 
 const Cards = (props) => {
   return (
     <div>
       <Card>
-        <CardImg top width="100%" src={image1} alt="Card image cap" />
+        <CardImg top width="100%" src={props.image} alt="Card image cap" />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
--- client/src/components/FluidJumbo/index.js
+++ client/src/components/FluidJumbo/index.js
@@ -1,6 +1,10 @@
+import {Component} from 'react';
 import React from 'react';
 import { Jumbotron, Container, Row, Col } from 'reactstrap';
 import Cards from '../Cards/cards.js';
+import image1 from '../../img/jump.jpg'
+import image2 from '../../img/ouch.jpg'
+import image3 from '../../img/hand.jpg'
 
 const FluidJumbo = (props) => {
   return (
@@ -8,9 +12,9 @@ const FluidJumbo = (props) => {
       <Jumbotron fluid>
         <Container fluid>
           <Row>
-            <Col sm="4"><Cards /></Col>
-            <Col sm="4"><Cards /></Col>
-            <Col sm="4"><Cards /></Col>
+            <Col sm="4"><Cards name='image1' image={image1}/></Col>
+            <Col sm="4"><Cards name='image2' image={image2}/></Col>
+            <Col sm="4"><Cards name='image3' image={image3}/></Col>
           </Row>
         </Container>
       </Jumbotron>
